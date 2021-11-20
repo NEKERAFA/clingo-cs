c22a4273dd731ff69c3c3eb7f5c2af4fb66eb19b
Rafael Alcalde Azpiazu <nekerafa@gmail.com>
2021-11-20 02:51:01 +0100

    Configured nuget package in ClingoSharp

--- build.ps1
+++ build.ps1
@@ -1,17 +1,17 @@
 Write-Host "Clearing working directory..." -ForegroundColor Green
 
-if (Test-Path ".\clingo\build\win")
+if (Test-Path "clingo\build\win")
 {
-    Write-Host "Deleting .\clingo\build\win..." -ForegroundColor Yellow
-    Remove-Item ".\clingo\build\win" -Recurse -ErrorAction Ignore
+    Write-Host "Deleting clingo\build\win..." -ForegroundColor Yellow
+    Remove-Item "clingo\build\win" -Recurse -ErrorAction Ignore
 }
 
 Write-Host ""
 
 Write-Host "Compiling clingo..." -ForegroundColor Green
 
-cmake "clingo" -B".\clingo\build\win" -DCLINGO_BUILD_SHARED=ON -DCLINGO_BUILD_WITH_PYTHON=OFF -DCLINGO_BUILD_WITH_LUA=OFF -DCLINGO_BUILD_APPS=OFF -G "Visual Studio 16 2019" -A x64
-cmake --build ".\clingo\build\win" --config Release
+cmake "clingo" -B"clingo\build\win" -DCLINGO_BUILD_SHARED=ON -DCLINGO_BUILD_WITH_PYTHON=OFF -DCLINGO_BUILD_WITH_LUA=OFF -DCLINGO_BUILD_APPS=OFF -G "Visual Studio 16 2019" -A x64
+cmake --build "clingo\build\win" --config Release
 
 Write-Host ""
 
