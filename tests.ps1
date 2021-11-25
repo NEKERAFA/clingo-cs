a67e1483507dea384eaf5623876c5078e52b1093
Rafael Alcalde Azpiazu <NEKERAFA@users.noreply.github.com>
2021-11-25 12:55:55 +0100

    Update tests.ps1

--- tests.ps1
+++ tests.ps1
@@ -1,29 +1,15 @@
-Write-Host "Clearing working directory..." -ForegroundColor Green
-
-if (Test-Path ".\tests\bin\Debug\netcoreapp3.1\runtimes\win\native\")
-{
-    Write-Host "Deleting .\tests\bin\Debug\netcoreapp3.1\runtimes\win\native\..." -ForegroundColor Yellow
-    Remove-Item ".\tests\bin\Debug\netcoreapp3.1\runtimes\win\native\" -Recurse -ErrorAction Ignore
-}
-
-if (Test-Path ".\tests\bin\Debug\netcoreapp3.1\files\")
-{
-    Write-Host "Deleting .\tests\bin\Debug\netcoreapp3.1\files\..." -ForegroundColor Yellow
-    Remove-Item ".\tests\bin\Debug\netcoreapp3.1\files\" -Recurse -ErrorAction Ignore
-}
-
 Write-Host "Copying clingo dependencies..." -ForegroundColor Green
 
-Copy-Item ".\clingo\build\win\bin\Release" ".\tests\bin\Debug\netcoreapp3.1\runtimes\win\native\" -Recurse
+Copy-Item "clingo\build\win\bin\Release\clingo.dll" "tests\bin\Debug\net6.0"
 
 Write-Host "Copying test files dependencies..." -ForegroundColor Green
 
-Copy-Item ".\tests\files" ".\tests\bin\Debug\netcoreapp3.1\files" -Recurse
+Copy-Item "tests\ClingoSharp.Tests\files" "tests\bin\Debug\net6.0" -Recurse
 
 Write-Host "Executing tests..." -ForegroundColor Green
 
-dotnet test /p:CollectCoverage=true /p:CoverletOutput=results/ /p:CoverletOutputFormat=lcov
-(Get-Content ".\tests\results\coverage.info") -replace [Regex]::Escape("$($PSScriptRoot)\"), [Regex]::Escape("") | Out-File -encoding ASCII "coverage.info"
+dotnet test --collect:"XPlat Code Coverage" --settings tests/ClingoSharp.Tests/coverlet.runsettings
+(Get-Content ".\tests\ClingoSharp.Tests\TestResults\*\coverage.info") -replace [Regex]::Escape("$($PSScriptRoot)\"), [Regex]::Escape("") | Out-File -encoding ASCII "coverage.info"
 
 Write-Host ""
 
